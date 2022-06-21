b7372f4cbb5802a42045f2fab8972abe27e14a84
jeffersonsamp46 <46848132+jeffersonsamp46@users.noreply.github.com>
2022-06-21 20:19:37 -0300

    configuracoes

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
-server.get('/curso', (req, res)=>{
-  //return res.send("Estou ficando fera")
-  return res.json( {"aprendizado": "Estou ficando fera"})
+const cursos = ['TypeScript', 'C++', 'Java', 'PHP']
+
+server.get('/cursos',(req,res)=>{
+  return res.json(cursos);
+});
+
+server.get('/cursos/:index', (req, res)=>{
+  const {index} = req.params;
+
+  return res.json(cursos[index]);
+});
+
+server.post('/cursos' , (req,res)=>{
+  const {name} = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+
+server.put('/cursos/:index', (req,res)=>{
+  const {index} = req.params;
+  const {name} = req.body;
+
+  cursos[index] = name;
+  return res.json(cursos);
 })
 
-server.listen(2000)
+server.delete('/cursos/:index',(req, res)=>{
+  const {index} = req.params;
+  cursos.splice(index, 1)
+
+  return res.json(cursos)
+})
+
+server.listen(2000);
