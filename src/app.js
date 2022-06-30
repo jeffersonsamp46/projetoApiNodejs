01b1c9746241ce9ff44b9bba4462dd8eaf6569b1
jeffersonsamp46 <46848132+jeffersonsamp46@users.noreply.github.com>
2022-06-30 20:30:41 -0300

    Ajustes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-const express  = require('express');
+import express from 'express'
+import mongoose from 'mongoose'
+import routes from './routes'
 
 class App{
+
   constructor(){
     this.server = express();
+
+    mongoose.connect('mongodb+srv://devhouse:devhouse@cluster0.nh1t2.mongodb.net/devhouse?retryWrites=true&w=majority',{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares(){
+    this.server.use(express.json())
+  }
+
+  routes(){
+    this.server.use(routes)
   }
 }
+
+export default new App().server
