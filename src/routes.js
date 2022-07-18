c924edd9b8bea501add1d846a10fca13a56c5dff
jeffersonsamp46 <46848132+jeffersonsamp46@users.noreply.github.com>
2022-07-18 19:41:05 -0300

    Api node

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import {Router} from 'express'
 import SessionController from './controller/SessionController'
+import HouseController from './controller/HouseController'
+
+import multer from 'multer'
+import uploadConfig from './config/upload'
 
 const routes = new  Router()
+const upload = multer(uploadConfig)
 
 routes.post('/session',SessionController.store)
+
+routes.post('/houses', upload.single('thumbnail'), HouseController.store)
 
 export default routes
