afbada88b25dcc26981e167dacaf8f0061f10c28
jeffersonsamp46 <46848132+jeffersonsamp46@users.noreply.github.com>
2022-07-19 21:08:44 -0300

    Api node: CRUD

--- src/controller/HouseController.js
+++ src/controller/HouseController.js
@@ -1,9 +1,71 @@
-class HouseController{
-    async store(req,res){
-        console.log(req.body)
-        console.log(req.file)
+import House from '../models/House'
+import User from '../models/User'
 
-        return res.json({ok: true})
+class HouseController{
+
+    async destroy(req, res){
+        const { house_id } = req.body
+        const { user_id } = req.headers
+
+        const user = await User.findById(user_id)
+        const houses = await House.findById(house_id)
+        if( String(user._id) !== String(houses.user)){
+            return res.status(401).json({error: 'Não Autorizado'})
+        }
+
+        await House.findByIdAndDelete({_id: house_id})
+
+        return res.json({message: "Excluida com sucesso"})
+    }
+
+    async update(req,res){
+        const { filename } = req.file
+        const { house_id } = req.params
+        const {description, price, location, status} = req.body
+        const { user_id } = req.headers
+
+        const user = await User.findById(user_id)
+        const houses = await House.findById(house_id)
+
+        if( String(user._id) !== String(houses.user)){
+            return res.status(401).json({error: 'Não Autorizado'})
+        }
+
+
+        await House.updateOne({_id: house_id }, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+        })
+
+        return res.send()
+    }
+
+    async index(req, res){
+        const { status } = req.query
+        const houses = await House.find({status})
+        return res.json({houses})
+    }
+
+    async store(req,res){
+        const { filename } = req.file
+        const {description, price, location, status} = req.body
+        const { user_id } = req.headers
+
+        const house = await House.create({
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            status,
+        })
+
+
+        return res.json({house})
     }
 }
 
